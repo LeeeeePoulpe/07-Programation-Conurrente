c06709e1b32a2bb615b522a703fcd168da1fab9c
LeeeeePoulpe <hfollmi@gmail.com>
2025-02-21 14:29:37 +0100

    TP02 exo 3

--- chamilo/code/tp02/batterie-partagee/Batterie.cpp
+++ chamilo/code/tp02/batterie-partagee/Batterie.cpp
@@ -11,17 +11,16 @@ Batterie::Batterie()
 void Batterie::frapper_decale(const unsigned long cote)
 {
     mutex_toms[cote].lock();
-    mutex_toms[1-cote].lock();
     toms[cote]+=1;    // un coup sur le tom à la gauche du batteur
-    toms[1-cote]+=2;  // deux coups sur le tom à la droite du batteur
     mutex_toms[cote].unlock();
+    mutex_toms[1-cote].lock();
+    toms[1-cote]+=2;  // deux coups sur le tom à la droite du batteur
     mutex_toms[1-cote].unlock();
 }
 
 void Batterie::frapper_ensemble(const unsigned long cote)
 {
-    mutex_toms[cote].lock();
-    mutex_toms[1-cote].lock();
+    std::lock(mutex_toms[cote], mutex_toms[1-cote]);
     toms[cote]+=1;  // un coup sur un tom
     toms[1-cote]+=1;  // un coup sur l'autre tom
     mutex_toms[cote].unlock();
